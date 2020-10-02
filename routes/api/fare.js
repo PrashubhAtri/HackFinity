5a3c838456a6a03d0a56616a8f822a5a58ca27d4
Prashubh Atri <prashubh27@gmail.com>
2020-10-02 19:09:56 +0530

    fare

---
+++ routes/api/fare.js
@@ -0,0 +1,42 @@
+const { Router } = require('express')
+const { check, validationResult } = require('express-validator')
+const axios = require('axios')
+const config = require('config')
+
+const route = Router();
+
+//Constants
+const CHARGEPERSTATION = config.get('CHARGEPERSTATION');
+const INTERCHANGECHARGES = config.get('INTERCHANGECHARGES');
+
+//Post Route for Booking a Slot
+/*
+    Body Requirement : {
+        initialStation : the Starting Station of the Journey,
+        finalStation : the Ending Station of the Journey,
+    }
+*/
+route.post('/', [
+    check('initialStation', 'Initial Station Field is Required').not().isEmpty(),
+    check('finalStation', 'Final Station Field is Required').not().isEmpty()
+],async (req, res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    try {
+        //Calculating the fare
+        const uri = encodeURI(
+            `https://delhimetroapi.herokuapp.com/metroapi/from=${req.body.initialStation}&to=${req.body.finalStation}`
+        )
+        const MetroResponse = await axios.get(uri);
+        const Data = MetroResponse.data;
+        let fare = CHARGEPERSTATION*(Data.path.length) + INTERCHANGECHARGES*(Data.interchange.length);
+        res.json({fare})
+    } catch (err) {
+        console.error(err.message)
+        return res.status(500).send("Server Error")
+    }
+})
+
+module.exports = { route }
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 
 const BookingsRoute = require('./bookings').route
+const FareRoute = require('./bookings').route
 
 const route = Router()
 
 route.use('/bookings',BookingsRoute)
+route.use('/fare',FareRoute)
 
 module.exports = { route }
